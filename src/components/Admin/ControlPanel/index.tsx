031ed2bbeda90cf0b97d5d1e2cc02ffe19e21ee3
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-07-13 16:13:33 +0800

    test: remove meetup, member and project modals

--- src/components/Admin/ControlPanel/index.tsx
+++ src/components/Admin/ControlPanel/index.tsx
@@ -1,6 +1,4 @@
 // import axios from "axios";
-import { NewMeetupActionButton } from "../ActionButton/NewMeetupActionButton";
-import { NewProjectActionButton } from "../ActionButton/NewProjectActionButton";
 import { NewUpdateActionButton } from "../ActionButton/NewUpdateActionButton";
 
 export default function ControlPanel() {
@@ -10,9 +8,6 @@ export default function ControlPanel() {
         <h2 className="text-3xl font-semibold mb-2">Control Panel</h2>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {/* <NewMemberActionButton /> */}
-        <NewMeetupActionButton />
-        <NewProjectActionButton />
         <NewUpdateActionButton />
       </div>
     </>
