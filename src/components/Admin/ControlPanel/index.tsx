bc491762d2efb2e9cf34a3a3b64dbb20cadf252d
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-07-13 16:04:16 +0800

    test: see if commenting the buttons helps mitigate the issue

--- src/components/Admin/ControlPanel/index.tsx
+++ src/components/Admin/ControlPanel/index.tsx
@@ -9,12 +9,12 @@ export default function ControlPanel() {
       <div className="flex items-center mt-10">
         <h2 className="text-3xl font-semibold mb-2">Control Panel</h2>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {/* <NewMemberActionButton /> */}
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+       
         <NewMeetupActionButton />
         <NewProjectActionButton />
         <NewUpdateActionButton />
-      </div>
+      </div> */}
     </>
   );
 }
